14013bfe1fe3bcc18f100d9a3aa4a3105d65614c
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2010-06-21 15:40:43 +0200

    release 0.8.1

--- build.upload.sh
+++ build.upload.sh
@@ -7,7 +7,7 @@ VERSION=$(dpkg-parsechangelog | awk '/^Version/ {print $2}')
 [ "$1" != "nobuild" ] && debuild -us -uc -I
 
 
-rsync -Pavz ../*${VERSION}* max.educa:/usr/local/max/logs/trac/incoming/branches/max-ebox/
+rsync --bwlimit=40 -Pavz ../*${VERSION}* max.educa:/usr/local/max/logs/trac/incoming/branches/max-ebox/
 ssh max.educa -t /usr/local/max/logs/root/bin/inject_incoming branches/max-ebox
 
 #rsync -Pavz ../*${VERSION}* root@thinetic.com:/var/www/virtual/thinetic.com/htdocs/max-control/
