c31af5fe7f53cbae5e978f69f53396eb6d812a62
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2010-06-22 14:49:59 +0200

    add backharddi-ng-text menu

--- build.upload.sh
+++ build.upload.sh
@@ -7,10 +7,9 @@ VERSION=$(dpkg-parsechangelog | awk '/^Version/ {print $2}')
 [ "$1" != "nobuild" ] && debuild -us -uc -I
 
 
-rsync --bwlimit=40 -Pavz ../*${VERSION}* max.educa:/usr/local/max/logs/trac/incoming/branches/max-ebox/
-ssh max.educa -t /usr/local/max/logs/root/bin/inject_incoming branches/max-ebox
+[ "$1" != "noupload" ] && rsync --bwlimit=40 -Pavz ../*${VERSION}* max.educa:/usr/local/max/logs/trac/incoming/branches/max-ebox/
+[ "$1" != "noupload" ] && ssh max.educa -t /usr/local/max/logs/root/bin/inject_incoming branches/max-ebox
 
-#rsync -Pavz ../*${VERSION}* root@thinetic.com:/var/www/virtual/thinetic.com/htdocs/max-control/
 
 
 fakeroot debian/rules clean
