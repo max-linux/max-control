5df1d684bf407d20bb4cfc97dcbd9bced75bf391
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2011-03-22 18:58:32 +0100

    allow to build EBox/Zentyal modules

---
+++ build.upload.zentyal.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+cp debian/control.zentyal debian/control
+VERSION=$(dpkg-parsechangelog | awk '/^Version/ {print $2}')
+
+
+[ "$1" != "nobuild" ] && debuild -us -uc -I
+
+
+[ "$1" != "noupload" ] && rsync --bwlimit=70 -Pavz ../*${VERSION}* max.educa.madrid.org:/usr/local/max/logs/trac/incoming/branches/max-zentyal/
+[ "$1" != "noupload" ] && ssh max.educa.madrid.org -t /usr/local/max/logs/root/bin/inject_incoming branches/max-zentyal
+
+
+
+fakeroot debian/rules clean
+rm -f debian/control
