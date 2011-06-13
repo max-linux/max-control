aa915213c35fc83be8ecc6a90c214e27a21f61da
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2011-06-13 22:42:26 +0200

    change upload 70 => 500

--- build.upload.sh
+++ build.upload.sh
@@ -8,7 +8,7 @@ VERSION=$(dpkg-parsechangelog | awk '/^Version/ {print $2}')
 [ "$1" != "nobuild" ] && debuild -us -uc -I
 
 
-[ "$1" != "noupload" ] && rsync --bwlimit=70 -Pavz ../*${VERSION}* max.educa:/usr/local/max/logs/trac/incoming/branches/max-ebox/
+[ "$1" != "noupload" ] && rsync --bwlimit=500 -Pavz ../*${VERSION}* max.educa:/usr/local/max/logs/trac/incoming/branches/max-ebox/
 [ "$1" != "noupload" ] && ssh max.educa -t /usr/local/max/logs/root/bin/inject_incoming branches/max-ebox
 
 
