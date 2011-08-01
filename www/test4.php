60645888ead1ae6d6e85747afe1f06cb51da7256
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2011-08-01 19:41:28 +0200

    www/test4.php: test is_teacher LDAP method

---
+++ www/test4.php
@@ -0,0 +1,39 @@
+<?php
+
+#ini_set("track_errors", 1);
+#ini_set("display_errors",1);
+#error_reporting(E_ERROR | E_WARNING | E_PARSE);
+#error_reporting(1);
+
+include("conf.inc.php");
+include('modules/common.inc.php');
+
+class GUI {
+    function debug($txt) {
+        if($txt == '') return;
+        echo "D: ".print_r($txt, true)." \n";
+    }
+    function debuga($txt) {
+        if($txt == '') return;
+        echo "D: ".print_r($txt, true)." \n";
+    }
+    function session_info($txt) {
+        $this->debug("SESSION INFO: ".$txt);
+    }
+    function session_error($txt) {
+        $this->debug("SESSION ERROR:".$txt);
+    }
+}
+
+$gui = new GUI();
+
+include("classes/ldap.class.php");
+
+$ldap=new LDAP();
+$gui->debuga($ldap->is_teacher('festeban'));
+
+
+
+
+
+?>
