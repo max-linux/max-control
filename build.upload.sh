6c1414dfb88f28959c87b1a4eb283dc0b41f9d28
Mario Izquierdo (mariodebian) <mariodebian@gmail.com>
2013-05-19 13:38:02 +0200

    Delete some ols EBox files

--- build.upload.sh
+++ build.upload.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 set -e
 
-cp debian/control.ebox debian/control
+[ "$1" != "nobuild" ] && rm -f ../max-control*deb
+cp debian/control.zentyal debian/control
 VERSION=$(dpkg-parsechangelog | awk '/^Version/ {print $2}')
 
 
 [ "$1" != "nobuild" ] && debuild -us -uc -I
 
 
-[ "$1" != "noupload" ] && rsync --bwlimit=500 -Pavz ../*${VERSION}* max.educa:/usr/local/max/logs/trac/incoming/branches/max-ebox/
-[ "$1" != "noupload" ] && ssh max.educa -t /usr/local/max/logs/root/bin/inject_incoming branches/max-ebox
+[ "$1" != "noupload" ] && rsync --bwlimit=500 -Pavz ../*${VERSION}* max.educa.madrid.org:/usr/local/max/html/max-server/max-server-2013/incoming
+[ "$1" != "noupload" ] && ssh max.educa.madrid.org -t /usr/local/max/html/max-server/max-server-2013/inject_incoming
 
 
 
 fakeroot debian/rules clean
 rm -f debian/control
+[ "$1" != "nobuild" ] && dpkg -i ../max-control*deb
